173acb68c59353b7d0152f8bf6ad7825a7156e5e
Felipe Barcon <felipe.barcon@gmail.com>
2022-08-13 18:10:54 +0200

    add selectPokemon and improove the function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,26 @@ import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Liste de Pokémons</h1> `,
+  templateUrl: 'app.component.html',
 })
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
 
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
+  selectPokemon(pokemonId: string) {
+    const id = +pokemonId;
+    const pokemon: Pokemon | undefined = this.pokemonList.find((pokemon) => pokemon.id == +pokemonId);
+
+    if (pokemon) {
+      console.log(`Vous avez demandé le pokemon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log("Vous avez demandé un pokemon qui n'existe pas");
+      this.pokemonSelected = pokemon;
+    }
   }
 }
